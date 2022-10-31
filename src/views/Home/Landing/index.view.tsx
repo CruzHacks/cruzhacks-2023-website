31b6eff99851d707ac8d6902cbe9f482e85ee587
sidhantas <sidhantasharma41@gmail.com>
2022-10-31 13:12:00 -0700

    completed landing, no moon

--- src/views/Home/Landing/index.view.tsx
+++ src/views/Home/Landing/index.view.tsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react"
+import React, { ReactComponentElement, useState } from "react"
 import { subscribeMailchimp } from "../../../utils/api"
 import { validateEmail } from "../../../utils/validate"
 import "./index.scss"
+import { ReactComponent as Arrow } from "../../../assets/Arrow.svg"
+import { ReactComponent as Instagram } from "../../../assets/Instagram.svg"
+import { ReactComponent as Facebook } from "../../../assets/Facebook.svg"
+import { ReactComponent as LinkedIn } from "../../../assets/Linkedin.svg"
+import { ReactComponent as Twitter } from "../../../assets/Twitter.svg"
+import { ReactComponent as Discord } from "../../../assets/Discord.svg"
 
 const SubmissionStates = {
   NotSubmitted: 0,
@@ -9,6 +15,16 @@ const SubmissionStates = {
   Submitted: 2,
   Errored: 3,
 }
+
+interface SocialButtonProps {
+  logo: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+}
+
+const SocialButton = (props: SocialButtonProps) => (
+  <button className='social_button'>
+    <props.logo className='social_button--svg' />
+  </button>
+)
 
 const Landing: React.FC = () => {
   const [email, setEmail] = useState<string>("")
@@ -37,40 +53,61 @@ const Landing: React.FC = () => {
     })
   }
   return (
-    <div className='landing__container'>
-      <div className='landing__container--title'>CruzHacks 2023</div>
-      <div className='landing__container--blurb'>A tiny blurb about us</div>
-      <div className='landing__container--when'>When is it</div>
-      <div className='landing__container--where'>Where is it</div>
-      <div className='landing__container--inputs'>
-        <div className='landing__container--inputs__row1'>
-          <input
-            className='landing__container--inputs__row1--button1'
-            onChange={e => setEmail(e.target.value)}
-          />
-          <button
-            className='landing__container--inputs__row1--button2'
-            onClick={() => handleSubmit()}
+    <div>
+      <div className='landing__container'>
+        <div className='landing__container--title'>CRUZHACKS 2023</div>
+        <div className='landing__container--blurb'>
+          CruzHacks is the largest hackathon in Santa Cruz. Each year, we invite
+          hundreds of students to develop solutions to real-world problems,
+          pursue inclusion in tech, and kindle the spirit of innovation.
+        </div>
+        <div className='landing__container--inputs'>
+          <div className='landing__container--inputs__row-container'>
+            <div className='landing__container--inputs__row-container__row1'>
+              <input
+                //eslint-disable-next-line max-len
+                className='landing__container--inputs__row-container__row1--email-input'
+                onChange={e => setEmail(e.target.value)}
+                placeholder='Enter Email for updates'
+              ></input>
+              {/*eslint-disable-next-line max-len*/}
+              <div className='landing__container--inputs__row-container__row1__arrow-container'>
+                <button
+                  // eslint-disable-next-line max-len
+                  className='landing__container--inputs__row-container__row1__arrow-container--arrow'
+                  onClick={() => handleSubmit()}
+                >
+                  <Arrow />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='landing__container--inputs__row2'>
+            <button className='landing__container--inputs__row2--button1'>
+              Sponsor Us
+            </button>
+            <button className='landing__container--inputs__row2--button2'>
+              Apply
+            </button>
+          </div>
+        </div>
+        {message && (
+          <div
+            className={
+              "landing__container--res" + (state === 3 ? "__error" : "")
+            }
           >
-            --&gt;
-          </button>
-        </div>
-        <div className='landing__container--inputs__row2'>
-          <button className='landing__container--inputs__row2--button1'>
-            Apps Open Dec
-          </button>
-          <button className='landing__container--inputs__row2--button2'>
-            Sponsor Us
-          </button>
-        </div>
+            {message}
+          </div>
+        )}
       </div>
-      {message && (
-        <div
-          className={"landing__container--res" + (state === 3 ? "__error" : "")}
-        >
-          {message}
-        </div>
-      )}
+      <div className='landing__socials'>
+        <SocialButton logo={Instagram} />
+        <SocialButton logo={Facebook} />
+        <SocialButton logo={LinkedIn} />
+        <SocialButton logo={Twitter} />
+        <SocialButton logo={Discord} />
+      </div>
     </div>
   )
 }
