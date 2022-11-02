c94fb321ec1f08fbc71f435f9882d30c104df074
sidhantas <sidhantasharma41@gmail.com>
2022-11-01 17:23:12 -0700

    created prize tracks

---
+++ src/Props/Tracks/props.ts
@@ -0,0 +1,47 @@
+/* eslint-disable max-len */
+import React, { ReactSVG, ReactSVGElement, SVGProps } from "react"
+import { ReactComponent as Beginner } from "../../assets/Beginner.svg"
+import { ReactComponent as Education } from "../../assets/Education.svg"
+import { ReactComponent as Healthcare } from "../../assets/Healthcare.svg"
+import { ReactComponent as Justice } from "../../assets/Justice.svg"
+import { ReactComponent as Sustainability } from "../../assets/Sustainability.svg"
+import { ReactComponent as UIUX } from "../../assets/UXIcon.svg"
+
+export interface PrizeTrackProps {
+  logo: React.FunctionComponent<SVGProps<SVGSVGElement>>
+  title: string
+  blurb: string
+}
+
+export const PrizeTrackInputs: Array<PrizeTrackProps> = [
+  {
+    logo: Sustainability,
+    title: "SUSTAINABILITY",
+    blurb:
+      "This category is dedicated to supporting sustainability and conservation efforts. How can we live harmoniously with our planet?",
+  },
+  {
+    logo: Healthcare,
+    title: "HEALTHCARE",
+    blurb:
+      "This category aspires to address healthcare disparities. How do we support mental & physical well-being with accessible services?",
+  },
+  {
+    logo: Justice,
+    title: "JUSTICE",
+    blurb:
+      "This category aims to drive innovation for civil liberty and social justice. How can we uphold truth & order in a (mis)information age?",
+  },
+  {
+    logo: Beginner,
+    title: "BEGINNER",
+    blurb:
+      "Hackathons are meant for everyone, especially our first-timers! We've created a prize category to reward teams composed of all first-time hackers.",
+  },
+  {
+    logo: UIUX,
+    title: "UI/UX",
+    blurb:
+      "Have a new paradigm for interaction design? Create a project that has an awesome user experience!",
+  },
+]
--- src/views/Home/Tracks/index.view.tsx
+++ src/views/Home/Tracks/index.view.tsx
@@ -1,8 +1,31 @@
 import React from "react"
 import "./index.scss"
+import { PrizeTrackProps, PrizeTrackInputs } from "../../../Props/Tracks/props"
+interface TrackCardProps {
+  SVG: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  title: string
+  blurb: string
+}
+
+const TrackCard = (props: TrackCardProps) => (
+  <div className='track-card'>
+    <div className='track-card__SVG'>
+      <props.SVG className='track-card__SVG--image' />
+    </div>
+    <div className='track-card__title'>{props.title}</div>
+    <div className='track-card__blurb'>{props.blurb}</div>
+  </div>
+)
 
 const Tracks: React.FC = () => (
-  <div className='tracks__container'>Prize Tracks</div>
+  <div>
+    <div className='tracks__title'>TRACKS</div>
+    <div className='tracks__container'>
+      {PrizeTrackInputs.map(({ logo, title, blurb }: PrizeTrackProps) => (
+        <TrackCard key={title} SVG={logo} title={title} blurb={blurb} />
+      ))}
+    </div>
+  </div>
 )
 
 export default Tracks
