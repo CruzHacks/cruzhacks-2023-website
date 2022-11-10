fa82ff0f05e7b79cee3ca73db6684e0a75506070
Alper deCarion <adecario@ucsc.edu>
2022-11-10 15:40:36 -0800

    LINTER

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import Home from "./views/Home/index.view"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
