d93cf88575c9fba86d9bab6aab8b9bf3b3fa7e33
Alper deCarion <adecario@ucsc.edu>
2022-11-11 22:09:27 -0800

    final

--- src/components/NightSky/index.tsx
+++ src/components/NightSky/index.tsx
@@ -1,11 +1,15 @@
 import React from "react"
-import ShootingStars from "./ShootingStars/ShootingStars"
+import Stars from "./Stars"
+import Shooters from "./Shooters"
+import Clouds from "./Clouds"
 import "./index.scss"
+
 const NightSky: React.FC = () => (
   <div className='NightSky__container'>
-    <ShootingStars />
-    <ShootingStars />
-    <ShootingStars />
+    <Stars />
+    <Shooters />
+    <Shooters />
+    <Clouds />
   </div>
 )
 
