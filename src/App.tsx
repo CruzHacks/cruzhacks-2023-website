77ad7532fdc5072ac02920d878598ebf6b37c9e7
Alper deCarion <adecario@ucsc.edu>
2022-11-14 19:41:57 -0800

    scroll fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ErrorView from "./views/Error/index.view"
 import Team from "./views/Team/index.view"
 import NavBar from "./components/NavBar"
 import "./App.scss"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import ThemeProvider from "./contexts/ThemeContext/ThemeContext"
 import Footer from "./components/Footer"
 import NightSky from "./components/NightSky"
@@ -13,6 +13,14 @@ import NightSky from "./components/NightSky"
 import "./App.scss"
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    })
+  }, [useLocation().pathname])
+
   return (
     <div className='app'>
       <Auth0ProviderWithHistory
--- src/components/NightSky/Stars/index.tsx
+++ src/components/NightSky/Stars/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Particles from "react-tsparticles"
+import "./index.scss"
 
 const Stars: React.FC = () => (
   <div className='stars'>
