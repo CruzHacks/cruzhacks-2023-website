8dda6c0dbba7db249fa1d38ff15fec0cf013b459
Alperdec <adecario@ucsc.edu>
2022-12-21 15:34:24 -0800

    fixes

--- src/views/Portal/index.view.tsx
+++ src/views/Portal/index.view.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { useAuth0 } from "@auth0/auth0-react"
 import { useNavigate, Outlet } from "react-router-dom"
-import Loading from "../../components/Loading"
 
 const Portal = () => {
   const { user } = useAuth0()
