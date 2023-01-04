6fb81a1407887d4f8e6ea9c43bacb30ca5e3382a
Alperdec <adecario@ucsc.edu>
2023-01-03 20:06:27 -0800

    initial commit

--- src/views/Home/Sponsors/index.view.tsx
+++ src/views/Home/Sponsors/index.view.tsx
@@ -1,12 +1,30 @@
 import React from "react"
-import { ReactComponent as SponsorsIcons } from "../../../assets/Sponsors.svg"
+import { SponsorsIconsLinks } from "../../../Props/Sponsors/props"
+import { nanoid } from "nanoid"
 import "./index.scss"
 
 const Sponsors: React.FC = () => (
   <div className='sponsors__container'>
     Thanks to our Sponsors!
     <div className='sponsors__container--icons'>
-      <SponsorsIcons className='sponsorsIcons' />
+      {SponsorsIconsLinks.map(tier => (
+        <div key={nanoid()} className='sponsors__container--icons__row'>
+          {tier.map(item => (
+            <a
+              key={nanoid()}
+              href={item.link}
+              target='_blank'
+              referrerPolicy='no-referrer'
+              title={item.alt}
+              rel='noreferrer'
+            >
+              {React.createElement(item.icon, {
+                className: "sponsorIcon",
+              })}
+            </a>
+          ))}
+        </div>
+      ))}
     </div>
   </div>
 )
