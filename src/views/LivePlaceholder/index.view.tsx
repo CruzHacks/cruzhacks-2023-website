c10a2594b1dfb39004112563b34771b534e89f44
Alperdec <adecario@ucsc.edu>
2023-01-06 03:09:27 -0800

    spinner

--- src/views/LivePlaceholder/index.view.tsx
+++ src/views/LivePlaceholder/index.view.tsx
@@ -1,26 +1,10 @@
-import React, { useState } from "react"
+import React from "react"
 import { ReactComponent as PhContent } from "../../assets/placeholder.svg"
 import "./index.scss"
 
 const LivePlaceholder: React.FC = () => {
-  const elem = document.getElementById("footer")
-  // get initial dims
-  let footerHeight = 0
-  if (elem) footerHeight = elem.offsetHeight
-
-  const [divHeight, setDivHeight] = useState(window.innerHeight - footerHeight)
-
-  React.useEffect(() => {
-    function handleResize() {
-      // resize dims
-      if (elem) setDivHeight(window.innerHeight - elem.offsetHeight)
-    }
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
-
   return (
-    <div className='lph__container' style={{ height: divHeight }}>
+    <div className='lph__container'>
       <PhContent className='ph-icon' />
     </div>
   )
