4dfbde40cfd8a011a2c3dac21e728510008c1321
Devan OBoyle <doboyle@ucsc.edu>
2023-01-11 17:29:18 -0800

    fixing dumb bug

--- src/views/Portal/Admin/UserManagement/components/table/index.tsx
+++ src/views/Portal/Admin/UserManagement/components/table/index.tsx
@@ -1,4 +1,4 @@
-import React from { useState, useEffect } "react"
+import React from "react"
 //import { useAuth0 } from "@auth0/auth0-react"
 //import getApplicants from "../api"
 import check from "../../../../../../images/icons/check.svg"
