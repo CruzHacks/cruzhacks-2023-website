da5dc449441e631d87f8ce9e501518e37f2a0ea4
Alper deCarion <adecario@ucsc.edu>
2023-01-15 18:48:37 -0800

    final commit, added api call back, removed dummy data

--- src/views/Portal/Admin/UserManagement/components/api.ts
+++ src/views/Portal/Admin/UserManagement/components/api.ts
@@ -5,13 +5,13 @@ import { Message } from "../../../../../contexts/PortalBanners/PortalBanner"
 export const getApplicants = async (
   getAccessTokenSilently: any,
   setBanner: Dispatch<Message>,
-  setApplicants: Dispatch<object>
+  setApplicants: Dispatch<Array<any>>
 ) => {
   try {
     const token = await getAccessTokenSilently()
     const getApplicantsAxiosRequest = {
       method: "get",
-      url: `${process.env.REACT_APP_ENDPOINT_URL}/applications`,
+      url: `${process.env.REACT_APP_ENDPOINT_URL}/application/applications`,
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": process.env.REACT_APP_CORS_ORIGIN || "",
--- src/views/Portal/Admin/UserManagement/components/table/index.tsx
+++ src/views/Portal/Admin/UserManagement/components/table/index.tsx
@@ -1,10 +1,12 @@
-import React from "react"
-//import { useAuth0 } from "@auth0/auth0-react"
-//import getApplicants from "../api"
+import React, { useEffect, useState } from "react"
+import { useAuth0 } from "@auth0/auth0-react"
+import getApplicants from "../api"
 import check from "../../../../../../images/icons/check.svg"
 import x from "../../../../../../images/icons/x.svg"
-//import { useBanner } from
-//"../../../../../../contexts/PortalBanners/PortalBanner"
+/* eslint-disable */
+import { useBanner } from "../../../../../../contexts/PortalBanners/PortalBanner"
+/* eslint-enable */
+import Spinner from "../../../../../../components/Spinner"
 import "./index.scss"
 
 import {
@@ -18,7 +20,7 @@ import {
   Paper,
   SxProps,
 } from "@mui/material"
-
+/*
 const createDummy = (
   id: string,
   name: string,
@@ -69,7 +71,7 @@ const rows = [
     "Not Applicable"
   ),
 ]
-
+*/
 const StyledTableRow = styled(TableRow)(() => ({
   "&:nth-of-type(even)": {
     backgroundColor: "#FAFBFF",
@@ -112,113 +114,114 @@ const handleStatus = (status: string) => {
   return "not_applicable"
 }
 
-const handleCheckin = (checkin: string) => {
-  if (checkin == "Checked In") {
-    return check
-  }
-  return x
+const handleCheckin = (checkin: boolean) => {
+  return checkin ? check : x
+}
+
+interface HackerProps {
+  id: string
+  name: string
+  checkedIn: boolean
+  status: string
+  // last_activity: string
 }
 
 const ManageTable = () => {
   // Uncomment when ready to debug API call issue
 
-  //const [applicants, setApplicants] = useState<object>()
-  //const { getAccessTokenSilently } = useAuth0()
-  //const [render, setRender] = useState<boolean>(false)
-  //const { setBanner } = useBanner()
+  const [applicants, setApplicants] = useState<Array<any>>([])
+  const { getAccessTokenSilently } = useAuth0()
+  const [render, setRender] = useState<boolean>(false)
+  const { setBanner } = useBanner()
 
-  /*
   useEffect(() => {
     getApplicants(getAccessTokenSilently, setBanner, setApplicants).then(() =>
       setRender(true)
     )
   }, [])
 
-  console.log(applicants)
-  */
-
-  //if (render) {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-        <TableHead>
-          <TableRow sx={{ bgcolor: "#FAFBFF" }}>
-            <TableCell sx={headStyle}>ID</TableCell>
-            <TableCell align='right' sx={headStyle}>
-              Name
-            </TableCell>
-            <TableCell sx={headStyle} align='right'>
-              Check In
-            </TableCell>
-            <TableCell sx={headStyle} align='right'>
-              Status
-            </TableCell>
-            <TableCell sx={headStyle} align='right'>
-              Role
-            </TableCell>
-            <TableCell sx={headStyle} align='right'>
-              Last Activity
-            </TableCell>
-            <TableCell sx={headStyle} align='right'>
-              Evaluation Progress
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((applicant, index) => (
-            <StyledTableRow key={index}>
-              <TableCell sx={plainCellStyle} component='th' scope='row'>
-                {applicant.id}
+  if (render) {
+    return (
+      <TableContainer className='hacker_table__container' component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow sx={{ bgcolor: "#FAFBFF" }}>
+              <TableCell sx={headStyle}>ID</TableCell>
+              <TableCell align='right' sx={headStyle}>
+                Name
               </TableCell>
-              <TableCell
-                sx={{
-                  fontFamily: "Nunito Sans",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  fontSize: 16,
-                  fontHeight: 22,
-                  color: "#6488E5",
-                }}
-                align='right'
-              >
-                {applicant.name}
+              <TableCell sx={headStyle} align='right'>
+                Check In
               </TableCell>
-              <TableCell sx={plainCellStyle} align='right'>
-                {
-                  <div className='check_in__container'>
-                    <div className='check_in__container--text'>
-                      {applicant.check_in}
-                    </div>
-                    <img src={handleCheckin(applicant.check_in)} />
-                  </div>
-                }
+              <TableCell sx={headStyle} align='right'>
+                Status
               </TableCell>
-              <TableCell align='right'>
-                <div
-                  className={`status__container-${handleStatus(
-                    applicant.status
-                  )}`}
+              <TableCell sx={headStyle} align='right'>
+                Role
+              </TableCell>
+              <TableCell sx={headStyle} align='right'>
+                Last Activity
+              </TableCell>
+              <TableCell sx={headStyle} align='right'>
+                Evaluation Progress
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {applicants.map((applicant: HackerProps, index: number) => (
+              <StyledTableRow key={index}>
+                <TableCell sx={plainCellStyle} component='th' scope='row'>
+                  {applicant.id}
+                </TableCell>
+                <TableCell
+                  sx={{
+                    fontFamily: "Nunito Sans",
+                    fontStyle: "normal",
+                    fontWeight: 400,
+                    fontSize: 16,
+                    fontHeight: 22,
+                    color: "#6488E5",
+                  }}
+                  align='right'
                 >
-                  <div className='status__container'>{applicant.status}</div>
-                </div>
-              </TableCell>
-              <TableCell sx={plainCellStyle} align='right'>
-                {applicant.role}
-              </TableCell>
-              <TableCell sx={plainCellStyle} align='right'>
-                {applicant.last_activity}
-              </TableCell>
-              <TableCell sx={plainCellStyle} align='right'>
-                {applicant.eval_prog}
-              </TableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                  {applicant.name}
+                </TableCell>
+                <TableCell sx={plainCellStyle} align='right'>
+                  {
+                    <div className='check_in__container'>
+                      <div className='check_in__container--text'>
+                        {applicant.checkedIn}
+                      </div>
+                      <img src={handleCheckin(applicant.checkedIn)} />
+                    </div>
+                  }
+                </TableCell>
+                <TableCell align='right'>
+                  <div
+                    className={`status__container-${handleStatus(
+                      applicant.status
+                    )}`}
+                  >
+                    <div className='status__container'>{applicant.status}</div>
+                  </div>
+                </TableCell>
+                {/*
+                <TableCell sx={plainCellStyle} align='right'>
+                  {applicant.last_activity}
+                </TableCell>
+                */}
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )
+  }
+  return (
+    <div className='table-spinner'>
+      <Spinner />
+    </div>
   )
-  //}
-  //return <div>Could Not Load Table</div>
 }
 
 export default ManageTable
