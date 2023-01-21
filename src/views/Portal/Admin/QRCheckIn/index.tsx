2e13ab53492f2e533901ee11ee3bf284773f4d05
Devan OBoyle <doboyle@ucsc.edu>
2023-01-20 23:18:31 -0800

    Fixing lint

--- src/views/Portal/Admin/QRCheckIn/index.tsx
+++ src/views/Portal/Admin/QRCheckIn/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { useAuth0 } from "@auth0/auth0-react"
+import React from "react"
+//import { useAuth0 } from "@auth0/auth0-react"
 import { QrReader } from "react-qr-reader"
 import "./index.scss"
 
